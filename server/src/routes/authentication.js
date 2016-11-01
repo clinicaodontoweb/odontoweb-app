97750c6922df287c877ac21f72df7ae4f1879372
andrecn13 <cnandre.nunes@gmail.com>
2016-11-01 02:58:46 +0100

    added new services to api

--- server/src/routes/authentication.js
+++ server/src/routes/authentication.js
@@ -1,7 +1,7 @@
 module.exports	=	function(app){
 	
 	var request	= require('request');
-	var service	= 'http://192.169.169.166:8000/autenticacao-service';
+	var service	= 'http://127.0.0.1:8001/autenticacao-service';
 	
 	app.post('/login', function (req, res) {
 		request.post({
@@ -15,5 +15,29 @@ module.exports	=	function(app){
 			}
 		});
 
+	});
+
+	app.get('/change/tenant/:id', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+		
+		request.get({url: service + '/auth/' + req.params.id}, function(error, response, body){
+			res.json(JSON.parse(body));
+		});
+	});
+
+	app.get('/me', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+		
+		request.get({url: service + '/me'}, function(error, response, body){
+			res.json(JSON.parse(body));
+		});
 	});
 }
--- server/src/routes/sigla.js
+++ server/src/routes/sigla.js
@@ -1,27 +1,52 @@
 module.exports	=	function(app){
 	
-	var request	= require('request');
-	var service	= 'http://192.169.169.166:3334/agenda-service/sigla';
-	
+	var request		= require('request');
+	var service		= 'http://127.0.0.1:3334/agenda-service/sigla';
+
 	app.post('/sigla', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+		
+
 		request.post({url: service, json: req.body }, function(error, response, body){
 			res.send(body);
 		});
 	});
 
 	app.put('/sigla', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+		
 		request.put({url: service, json: req.body }, function(error, response, body){
 			res.send(body);
 		});
 	});
 
 	app.get('/sigla', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+
 		request.get({url: service}, function(error, response, body){
 			res.json(JSON.parse(body));
 		});
 	});
 
 	app.get('/sigla/:id', function (req, res) {
+		request = request.defaults({
+			headers: {
+				'X-AUTH-TOKEN': req.get('X-AUTH-TOKEN')
+			}
+		});
+		
 		request.get({url: service + '/' + req.params.id}, function(error, response, body){
 			res.json(JSON.parse(body));
 		});
